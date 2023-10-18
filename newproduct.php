4fb44b6e0d5d730c6eca8f0c5b6b833e05c18c3a
CYinnC <131159415+CYinnC@users.noreply.github.com>
2023-10-18 17:32:25 +0800

    Add files via upload

--- newproduct.php
+++ newproduct.php
@@ -4,7 +4,7 @@
 
 
 <head>
-<title>Admin Dashboard</title>
+<title>Admin Product</title>
 <link rel="stylesheet" href="astyle.css">
 </head>
 <body>
@@ -18,6 +18,22 @@
 			<a href="index.php">Statistic</a>
 			<a href="index.php">Logout</a>
 <div>
+
+
+<div class="layout">
+<h1>Add Product</h1>
+	<form action="#" method="post" >
+		<p>Product name:</p>
+		<input type="text" name="pname" placeholder="Name">
+		<p>Price:</p>
+		<input type="text" name="price" placeholder="RM0.00">
+		<p>Image:</p>
+		<input type="file" name="image" id="files">
+		<label for="files">Choose Image</label>
+		<br/><br/>
+		<button type="submit">Add Product</button>
+	</form>
+</div>
 
 
 </body>
