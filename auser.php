ad3357d624bd2ff37467731cce3ae29ca270d543
CYinnC <131159415+CYinnC@users.noreply.github.com>
2023-11-15 19:00:11 +0800

    Update auser.php

--- auser.php
+++ auser.php
@@ -1,6 +1,4 @@
-<?php
-	require("session_handling.php");
-?>
+
 
 
 <!DOCTYPE html>
@@ -17,8 +15,8 @@
 					<a href="admin.php">Dashboard</a>
 					<a href="apselect.php">Product</a>
 					<a href="auser.php">Users</a>
-					<a href="index.php">Statistic</a>
-					<a href="index.php">Logout</a>
+					<a href="areport.php">Statistic</a>
+					<a href="logout.php">Logout</a>
 		</div>
 
 
